0371af4409ad689e56b8967f62382a9a8d88830a
Kelly Shen <kellyshenfrog@gmail.com>
2021-05-31 01:59:52 -0500

    better rotation and scaling

--- src/components/Stars.tsx
+++ src/components/Stars.tsx
@@ -1,20 +1,35 @@
-import React, { useMemo, useRef } from "react";
+import React, { useMemo, useRef, useState } from "react";
 import * as THREE from "three";
 import { useFrame } from '@react-three/fiber';
 import { useTexture } from "@react-three/drei";
 
 const MAX_POINTS = 3000;
+const MAX_VELOCITY = 10;
 
 const Stars = () => {
 
+    const [scrolled, setScrolled] = useState(false);
     const points = useRef<THREE.Points>();
     let theta = 0;
+    let velocity = 0;
+    let acceleration = .5;
     useFrame(() => {
     if (points.current) {
-        const r = 5 * Math.sin(THREE.MathUtils.degToRad((theta -= 0.002)));
-        const s = Math.cos(THREE.MathUtils.degToRad(theta * 10));
-        points.current.rotation.set(r, r, r);
+        const r = 5 * Math.sin(THREE.MathUtils.degToRad((theta * .01)));
+        let s = Math.cos(THREE.MathUtils.degToRad(theta -= .5)) + 1.5;
+        points.current.rotation.set(0, r, 0);
         points.current.scale.set(s, s, s);
+
+        if(scrolled && points.current){
+            if( velocity < MAX_VELOCITY){
+                velocity += acceleration;
+            }
+            points.current.translateY(velocity);
+            if(points.current.position.y > 400){
+                points.current.position.y = -100;
+                acceleration = 0;
+              }
+          }
     }
   });
 
@@ -37,7 +52,7 @@ const Stars = () => {
         <bufferAttribute attachObject={["attributes", "position"]} count={positions.length / 3} array={positions} itemSize={3} />
         {/* <bufferAttribute ref={attrib} attachObject={["attributes", "color"]} count={colors.length / 3} array={colors} itemSize={3} /> */}
       </bufferGeometry>
-      <pointsMaterial attach="material" color="#ffddaa" size={1.0} sizeAttenuation={false} />
+      <pointsMaterial attach="material" color="#ffddaa" size={2.0} sizeAttenuation={false} />
     </points>
   );
 };
